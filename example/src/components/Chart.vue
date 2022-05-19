e0b78eae263e84373447fd3867f7731c673315c8
liutsing <liutsingluo@gmail.com>
2022-05-19 23:04:51 +0800

    docs: ~

--- example/src/components/Chart.vue
+++ example/src/components/Chart.vue
@@ -1,4 +1,14 @@
-<template>
+<template>this is chart</template>
+<script lang="ts">
+import { createContext, line, rect } from '@maple/sparrow'
+import { defineComponent } from 'vue'
 
-</template>
-<script></script>
+export default defineComponent({
+  mounted() {
+    const context = createContext(600, 600)
+    const _line = rect(context, { x: 0, y: 0, width: 20, height: 40 })
+    // const root = context.node
+    document.body.appendChild(_line)
+  },
+})
+</script>
