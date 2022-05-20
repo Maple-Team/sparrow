0afa8d309b9f192a843a07d1f9a7792aa70fd615
liutsing <liutsingluo@gmail.com>
2022-05-20 10:38:16 +0800

    docs: ~

--- example/src/components/Chart.vue
+++ example/src/components/Chart.vue
@@ -1,14 +1,21 @@
-<template>this is chart</template>
+<template>
+  <div class="container" ref="container"></div>
+</template>
 <script lang="ts">
-import { createContext, line, rect } from '@maple/sparrow'
+import { createContext, rect } from '@maple/sparrow'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   mounted() {
-    const context = createContext(600, 600)
-    const _line = rect(context, { x: 0, y: 0, width: 20, height: 40 })
-    // const root = context.node
-    document.body.appendChild(_line)
+    const context: Context = createContext(600, 600)
+    const rectEle = rect(context, {
+      x: 0,
+      y: 0,
+      width: 20,
+      height: 40,
+      fill: 'red',
+    })
+    this.$refs.container.appendChild(context.node)
   },
 })
 </script>
